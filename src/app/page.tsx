37cc895ee54cf92df28a0545128791fd8e4212e9
Dimas Abdullah Mufid <dimasmoveit@gmail.com>
2025-03-07 20:29:50 +0700

    add header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Header from "@/components/Header";
 import Stopwatch from "@/components/Stopwatch";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { db } from "@/db/schema";
@@ -7,23 +8,26 @@ export default async function Home() {
   const tasks = await db.select().from(schema.tasks);
 
   return (
-    <main className="flex min-h-screen flex-row">
-      <div className="w-1/2 p-6 border-r">
-        <Stopwatch />
-      </div>
-      <div className="w-1/2 p-6">
-        <Tabs defaultValue="tasks">
-          <TabsList>
-            <TabsTrigger value="tasks">Tasks</TabsTrigger>
-            <TabsTrigger value="projects">Projects</TabsTrigger>
-          </TabsList>
-          <TabsContent value="tasks">
-            {tasks.map((task) => (
-              <div key={task.id}>{task.name}</div>
-            ))}
-          </TabsContent>
-          <TabsContent value="projects">Projects</TabsContent>
-        </Tabs>
+    <main className="flex min-h-screen flex-col">
+      <Header />
+      <div className="flex flex-row grow">
+        <div className="w-1/2 p-6 border-r">
+          <Stopwatch />
+        </div>
+        <div className="w-1/2 p-6">
+          <Tabs defaultValue="tasks">
+            <TabsList>
+              <TabsTrigger value="tasks">Tasks</TabsTrigger>
+              <TabsTrigger value="projects">Projects</TabsTrigger>
+            </TabsList>
+            <TabsContent value="tasks">
+              {tasks.map((task) => (
+                <div key={task.id}>{task.name}</div>
+              ))}
+            </TabsContent>
+            <TabsContent value="projects">Projects</TabsContent>
+          </Tabs>
+        </div>
       </div>
     </main>
   );
